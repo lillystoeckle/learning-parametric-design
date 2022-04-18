ce22b734ee4a5f315f26e823cf665ef5c0a0fda3
lillystoeckle <lilly.stoeckle@fh-potsdam.de>
2022-04-18 11:18:59 +0200

    task-03

--- code/task-02/sketch.js
+++ code/task-02/sketch.js
@@ -1,22 +1,20 @@
-const circleSize = 10;
-
+let boxSize = 10;
 
 function preload(){
   // preload assets
 }
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400);  
 }
 
 function draw() {
-  background(255);
-  fill(0);
-
-  for (let x = 1; <= 10; x +- 1) {
-    for (let y = 1; y <= 10; y +- 1) {
-      circle(20 * x, 20 * y, circleSize);
+  strokeWeight(0);
+  
+    for (let x = 0; x < width; x+=boxSize){
+    for (let y = 0; y < height; y+=boxSize){
+      fill(random(255));
+      rect(x,y,boxSize,boxSize)
     }
   }
-
 }
