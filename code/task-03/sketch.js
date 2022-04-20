e36544d428b8737130f374a3a3a16529f0437a1b
lillystoeckle <lilly.stoeckle@fh-potsdam.de>
2022-04-20 12:16:23 +0200

    Update sketch.js

--- code/task-03/sketch.js
+++ code/task-03/sketch.js
@@ -7,40 +7,38 @@ function setup() {
 
 function draw(){
   background(255);
+  frameRate (5);
 
   strokeWeight(0);
   fill('rgba(255, 20, 80, 0.1)');
   beginShape();
   for(let angle = 0; angle < 360; angle += 15) {
-    const radius = random(150, 140);
+    const radius = random(80, 70);
     const x = radius * cos(Math.PI / 180 * angle);
     const y = radius * sin(Math.PI / 180 * angle);
     curveVertex(x + sketchWidth / 2, y + sketchHeight / 2);
   }
   endShape(CLOSE);
-  noLoop();
 
   strokeWeight(0);
   fill('rgba(255, 20, 80, 0.1)');
   beginShape();
   for(let angle = 0; angle < 360; angle += 15) {
-    const radius = random(160, 150);
+    const radius = random(90, 80);
     const x = radius * cos(Math.PI / 180 * angle);
     const y = radius * sin(Math.PI / 180 * angle);
     curveVertex(x + sketchWidth / 2, y + sketchHeight / 2);
   }
   endShape(CLOSE);
-  noLoop();
 
   strokeWeight(0);
   fill('rgba(255, 20, 80, 0.05)');
   beginShape();
   for(let angle = 0; angle < 360; angle += 15) {
-    const radius = random(170, 160);
+    const radius = random(100, 90);
     const x = radius * cos(Math.PI / 180 * angle);
     const y = radius * sin(Math.PI / 180 * angle);
     curveVertex(x + sketchWidth / 2, y + sketchHeight / 2);
   }
   endShape(CLOSE);
-  noLoop();
 }
