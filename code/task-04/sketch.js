2ae2c3ca295ea61a1eaec4166dd419b974052c18
lillystoeckle <lilly.stoeckle@fh-potsdam.de>
2022-04-26 18:55:22 +0200

    Update sketch.js

--- code/task-04/sketch.js
+++ code/task-04/sketch.js
@@ -13,12 +13,13 @@ function setup() {
 function draw() {
   background('rgba(214, 221, 255, 100)');
 
-  //Rechteck Querformat
-
-  let xpos = random(width);
-  let ypos = random(height);
+  let xpos = random(0, width);
+  let ypos = random(0, height);
 
   rect(xpos, ypos, 200, 100);
     fill('rgba(107, 122, 255, 0.5)');
     strokeWeight(0);
+
+//Random Hoch- und Querformat?
+// Warum selbe "random "x und y Koordinaten wenn code dubliziert?
 }
