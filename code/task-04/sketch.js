72dbf60ef63a7ab1df297097965e915acaa4acd6
lillystoeckle <lilly.stoeckle@fh-potsdam.de>
2022-04-26 17:58:23 +0200

    Update sketch.js

--- code/task-04/sketch.js
+++ code/task-04/sketch.js
@@ -1,20 +1,24 @@
+const sketchWidth = 400;
+const sketchHeight = 400;
+
 function preload(){
   // preload assets
 }
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(sketchWidth, sketchHeight);
+  frameRate (5);
 }
 
 function draw() {
-  frameRate (5);
-
   background('rgba(214, 221, 255, 100)');
+
+  //Rechteck Querformat
 
   let xpos = random(width);
   let ypos = random(height);
 
-  rect(xpos, ypos, 200, 100);s
-    fill('rgba(107, 122, 255, 100)');
+  rect(xpos, ypos, 200, 100);
+    fill('rgba(107, 122, 255, 0.5)');
     strokeWeight(0);
 }
