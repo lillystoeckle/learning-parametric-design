4f51aba1ba17f05fa73ba32cf3d38a6ce11f83f9
lillystoeckle <lilly.stoeckle@fh-potsdam.de>
2022-04-27 12:59:12 +0200

    Update sketch.js

--- code/task-05/sketch.js
+++ code/task-05/sketch.js
@@ -1,42 +1,44 @@
 const sketchWidth = 400;
 const sketchHeight = 400;
 
+let angleOffset = 0;
+
 function preload(){
   // preload assets
 }
 
 function setup() {
   createCanvas(sketchWidth, sketchHeight);
-  frameRate (5);
+  frameRate ();
 }
 
 function draw() {
-  background('rgba(214, 221, 255, 100)');
+  background('rgba(214, 221, 255, 1)');
 
   //Circle
   
-  fill('rgba(200, 2200, 200, 100)');
+  fill(255);
   strokeWeight(0);
   circle (200, 200, 40);
 
   //Rotorblatt 1
 
-  stroke('rgba(200, 2200, 200, 100)')
-  strokeWeight(2);
-  beginShape();
+  stroke(255, 100)
+  strokeWeight(4);
 
   for(let angle = 0; angle < 360; angle += 90) {
     const radius = (200, 150);
     const iradius = 21;
 
-    const x = radius * cos(Math.PI / 180 * angle);
-    const y = radius * sin(Math.PI / 180 * angle);
+    const x = radius * cos(Math.PI / 180 * (angle + angleOffset));
+    const y = radius * sin(Math.PI / 180 * (angle + angleOffset));
 
-    const ix = iradius * cos(Math.PI / 180 * angle);
-    const iy = iradius * sin(Math.PI / 180 * angle);
+    const ix = iradius * cos(Math.PI / 180 * (angle + angleOffset));
+    const iy = iradius * sin(Math.PI / 180 * (angle + angleOffset));
     
     line(x + 200, y + 200, ix +200, iy+200);
   }
-  endShape();
+
+  angleOffset += 1;
 
 }
