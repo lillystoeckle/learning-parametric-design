9ca637798059574851f8645d6e2ae6024edf79aa
lillystoeckle <lilly.stoeckle@fh-potsdam.de>
2022-04-27 12:41:28 +0200

    Update sketch.js

--- code/task-05/sketch.js
+++ code/task-05/sketch.js
@@ -12,4 +12,31 @@ function setup() {
 
 function draw() {
   background('rgba(214, 221, 255, 100)');
+
+  //Circle
+  
+  fill('rgba(200, 2200, 200, 100)');
+  strokeWeight(0);
+  circle (200, 200, 40);
+
+  //Rotorblatt 1
+
+  stroke('rgba(200, 2200, 200, 100)')
+  strokeWeight(2);
+  beginShape();
+
+  for(let angle = 0; angle < 360; angle += 90) {
+    const radius = (200, 150);
+    const iradius = 21;
+
+    const x = radius * cos(Math.PI / 180 * angle);
+    const y = radius * sin(Math.PI / 180 * angle);
+
+    const ix = iradius * cos(Math.PI / 180 * angle);
+    const iy = iradius * sin(Math.PI / 180 * angle);
+    
+    line(x + 200, y + 200, ix +200, iy+200);
+  }
+  endShape();
+
 }
