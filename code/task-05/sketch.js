0110606d10d1d1938781bbf0f1906046142b57c4
lillystoeckle <lilly.stoeckle@fh-potsdam.de>
2022-04-27 12:16:47 +0200

    task-05

---
+++ code/task-05/sketch.js
@@ -0,0 +1,15 @@
+const sketchWidth = 400;
+const sketchHeight = 400;
+
+function preload(){
+  // preload assets
+}
+
+function setup() {
+  createCanvas(sketchWidth, sketchHeight);
+  frameRate (5);
+}
+
+function draw() {
+  background('rgba(214, 221, 255, 100)');
+}
